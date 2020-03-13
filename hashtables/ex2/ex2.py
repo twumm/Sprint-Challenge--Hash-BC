d0854d26166f61a20bf814e8ee2e48f14318891c
Martin Twum Mensah <martint.mensah@gmail.com>
2020-03-13 10:16:36 +0000

    chore: Complete exercise 2 on hashtable

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,19 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # set a current location of none
+    # insert tickets into hashtable
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    # get first ticket with NONE as source
+    # first_ticket = hash_table_retrieve(hashtable, 'NONE')
+    # set that ticket in the route list as the first index
+    # route[0] = first_ticket
+    # set a location user is in
+    user_location = "NONE"
+    for i in range(length):
+        route[i] = hash_table_retrieve(hashtable, user_location)
+        user_location = route[i]
+
+    return route[:-1]
